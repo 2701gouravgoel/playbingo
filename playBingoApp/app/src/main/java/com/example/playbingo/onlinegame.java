f6156efda02c4c86fdb90ad441503cbf7f5a1577
2701gouravgoel <2701gouravgoel@gmail.com>
2020-06-29 23:29:28 +0530

    one more

--- playBingoApp/app/src/main/java/com/example/playbingo/onlinegame.java
+++ playBingoApp/app/src/main/java/com/example/playbingo/onlinegame.java
@@ -2,13 +2,86 @@ package com.example.playbingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 public class onlinegame extends AppCompatActivity {
+
+    private EditText typedmessage;
+    private ImageButton sendmessage;
+    private Socket msocket;
+    {
+        try {
+            msocket = IO.socket("https://obscure-reaches-99859.herokuapp.com/");
+        } catch (URISyntaxException e) {}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onlinegame);
+
+
+        intializedfields();
+
+        msocket.on("gamemessage",onmss);
+
+        sendmessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msocket.connect();
+                String msg = typedmessage.getText().toString();
+                JSONObject info = new JSONObject();
+                try{
+                    info.put("message",msg);
+                    msocket.emit("gamechat",info);
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private Emitter.Listener onmss = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout = inflater.inflate(R.layout.custum_toast, null);
+                    TextView text = (TextView) layout.findViewById(R.id.meage);
+
+                    text.setText(args[0].toString());
+                    Toast toast = new Toast(onlinegame.this);
+                    toast.setDuration(Toast.LENGTH_SHORT);
+                    toast.setView(layout);
+                    toast.show();
+                }
+            });
+        }
+    };
+
+    private void intializedfields()
+    {
+        typedmessage=(EditText)findViewById(R.id.send_message_in_game);
+        sendmessage=(ImageButton)findViewById(R.id.send_message_game);
     }
 }
